48810f5a4528b2a8543fbbe4cd555b93e36d0692
robo-codes <61969933+robo-codes@users.noreply.github.com>
2020-04-28 16:02:07 +0530

    basics of numpy

---
+++ numpybase.py
@@ -0,0 +1,14 @@
+import numpy as np
+a=n.array([1,2,3])
+list= [1,2,3]
+b=list*2
+c=a*2
+print (c)
+print (b)
+
+#ar=n.array([[1,2],[1,2]])
+#print(ar)
+mt=np.matrix([[1,2],[1,2]])
+print(mt)
+ar=np.array(mt)
+print(ar)
