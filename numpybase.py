6f288889de976dfa1c1c61efff9298fe2a61b5e3
robo-codes <61969933+robo-codes@users.noreply.github.com>
2020-04-28 16:38:56 +0530

    somethng more on numpy

--- numpybase.py
+++ numpybase.py
@@ -1,5 +1,5 @@
 import numpy as np
-a=n.array([1,2,3])
+a=np.array([1,2,3])
 list= [1,2,3]
 b=list*2
 c=a*2
@@ -8,7 +8,20 @@ print (b)
 
 #ar=n.array([[1,2],[1,2]])
 #print(ar)
-mt=np.matrix([[1,2],[1,2]])
+'''mt=np.matrix([[1,2],[1,2]])
 print(mt)
 ar=np.array(mt)
 print(ar)
+arr=np.random.random((1,2))
+print(arr)
+arrr=np.random.randn(1,3)
+print (arrr)
+#print (arr.mean())
+#print (arr.var())
+ar1=arr.dot(arrr)
+print(ar1)'''
+ar=np.array([[1,2,3],[2,3,1]])
+arr=np.array([[2,3,4],[1,2,3]])
+print(ar*arr)
+ar1=arr.T
+print(ar.dot(ar1))
