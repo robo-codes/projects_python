f4a8dfe38b38af387259407d7aff706111f74a3b
robo-codes <61969933+robo-codes@users.noreply.github.com>
2020-05-09 16:18:01 +0530

    reccursion hackerrank

--- hackerrank/lustyrobo.py
+++ hackerrank/lustyrobo.py
@@ -315,3 +315,33 @@ while True:
             print("Not found")
     except EOFError:
             break
+
+'''factorial reccursion'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(n):
+    fact=0
+    if n<=1:
+        return(1)
+    for i in range(n,1,-1):
+        fact=n*factorial(n-1)
+    return(fact)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
