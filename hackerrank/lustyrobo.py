5b65d2c72bb4c4fcb777d26fe2813b0db1c1af7f
robo-codes <61969933+robo-codes@users.noreply.github.com>
2020-05-09 17:08:36 +0530

    day 10 hackkerank

--- hackerrank/lustyrobo.py
+++ hackerrank/lustyrobo.py
@@ -345,3 +345,31 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
+'''day 10 hackerrank'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    b=(bin(n)[2:])
+    count=0
+    max_count=0
+    for i in b:
+        if i=='1':
+            count+=1
+        else:
+            if count>max_count:
+                max_count==count
+            count=0
+    if count>max_count:
+        max_count=count
+    print(max_count)
