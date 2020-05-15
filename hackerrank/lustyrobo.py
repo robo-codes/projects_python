b57c6206ca26a5fdc352b4e68a09eb01ebcd055e
robo-codes <61969933+robo-codes@users.noreply.github.com>
2020-05-15 15:20:05 +0530

    day 13 abstract class

--- hackerrank/lustyrobo.py
+++ hackerrank/lustyrobo.py
@@ -437,3 +437,30 @@ class Student(Person):
         else:
             return("O")
 line = input().split()
+
+
+'''day 13 abstract class'''
+
+from abc import ABCMeta, abstractmethod
+class Book(object, metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+    @abstractmethod
+    def display(): pass
+
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self,title,author,price):
+        super().__init__(title,author)
+        self.price=price
+    def display(self):
+        print("Title: "+title)
+        print("Author: "+author)
+        print("Price: "+str(price))
+
+title=input()
+author=input()
+price=int(input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
