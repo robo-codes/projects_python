574bf50bc8cfbacc6cf96fcb7cd56484eaec4971
robo-codes <61969933+robo-codes@users.noreply.github.com>
2020-05-18 16:02:12 +0530

    day 17 more exceptions

--- hackerrank/lustyrobo.py
+++ hackerrank/lustyrobo.py
@@ -532,3 +532,24 @@ try:
     print(S)
 except:
     print("Bad String")
+
+
+'''day 17 more exception'''
+
+#Write your code here
+class Calculator(object):
+    def power(self,n,p):
+        if (n < 0) or (p < 0):
+            raise Exception('n and p should be non-negative')
+        else:
+            return pow(n,p)
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
