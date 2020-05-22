f00985f85fed4d6e78fe13b846ea8b760c481bf8
robo-codes <61969933+robo-codes@users.noreply.github.com>
2020-05-22 15:08:29 +0530

    some changes in model selection

--- machine_learning/first_model.py
+++ machine_learning/first_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+#from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 house_rent_file_path = r'C:\machine_learning\houseRent\housing_train.csv'
@@ -8,10 +9,8 @@ features = ['sqfeet', 'beds', 'baths']
 X = home_data[features]
 y = home_data['price']
 train_X, valid_X, train_y, valid_y = train_test_split(X, y, random_state=1)
-homies_model = DecisionTreeRegressor(random_state=1)
+homies_model = RandomForestRegressor(n_estimators=100, random_state=1)
 homies_model.fit(train_X,train_y)
 
 preds = homies_model.predict(valid_X)
-print(homies_model.predict(valid_X.head()))
-print(valid_y.head().tolist())
-print(mean_absolute_error(valid_y,preds))
+print(mean_absolute_error(preds, valid_y))
