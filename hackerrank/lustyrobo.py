b257c03aec083a12de9f4aecf0378fff9da5b4e8
robo-codes <61969933+robo-codes@users.noreply.github.com>
2020-05-22 15:42:24 +0530

    day 23 hackerrank bst level traversal

--- hackerrank/lustyrobo.py
+++ hackerrank/lustyrobo.py
@@ -723,3 +723,44 @@ for i in range(T):
     root=myTree.insert(root,data)
 height=myTree.getHeight(root)
 print(height)
+
+
+'''day 23 binary search tree level traversal'''
+
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        if root:
+            queue = [root]
+            while queue:
+                node = queue.pop()
+                print(node.data, end=" ")
+                if node.left:
+                    queue.insert(0, node.left)
+                if node.right:
+                    queue.insert(0, node.right)
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
