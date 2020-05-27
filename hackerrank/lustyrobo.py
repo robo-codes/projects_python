fa5ef3820c9502dcb23bd38ec81a5c13464ea438
robo-codes <61969933+robo-codes@users.noreply.github.com>
2020-05-27 16:24:02 +0530

    Day 28: RegEx, Patterns, and Intro to Databases hackerrank

--- hackerrank/lustyrobo.py
+++ hackerrank/lustyrobo.py
@@ -939,3 +939,29 @@ TestWithEmptyArray()
 TestWithUniqueValues()
 TestiWithExactyTwoDifferentMinimums()
 print("OK")
+
+'''Day 28: RegEx, Patterns, and Intro to Databases'''
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    arr = []
+    for N_itr in range(N):
+        firstNameEmailID = input().split()
+
+        firstName = firstNameEmailID[0]
+
+        emailID = firstNameEmailID[1]
+
+        if "@gmail.com" in emailID:
+            arr.append(firstName)
+    arr.sort()
+    for i in arr:
+        print(i)
