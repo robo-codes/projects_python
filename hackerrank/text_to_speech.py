d802816cae05104222ab59c109df234a022a8ade
robo-codes <61969933+robo-codes@users.noreply.github.com>
2020-06-01 02:07:06 +0530

    text to speech first try !

--- hackerrank/text_to_speech.py
+++ hackerrank/text_to_speech.py
@@ -1,6 +1,13 @@
 from gtts import gTTS
 import os
-text = "love yourself brother! "
+text = str(input())
+#print(googletrans.LANGUAGES)
+'''translator = Translator()
+translated = translator.translate(text, src = 'en', dest = 'gu')
+#print(translated.text)
+speech = gTTS(text = text, lang = language, slow = False)
+speech.save("test.mp3")
+os.system("start test.mp3")'''
 language = 'en'
 speech = gTTS(text = text, lang = language, slow = False)
 speech.save("test.mp3")
